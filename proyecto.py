097597c837ef8fc4f1bf3d0026ed04a2839dcc96
Eduardo Mora <99228341+EduardoGMora@users.noreply.github.com>
2023-11-13 12:58:20 -0600

    estructura de datos
    Aquí hice las estructuras de cómo se iban a guardar cada objeto

---
+++ proyecto.py
@@ -0,0 +1,62 @@
+#profesores
+class Profesor:
+    def __init__(self, id_profesor, nombre, horarios_disp):
+        self.id = id_profesor
+        self.nombre = nombre
+        self.horarios_disp = horarios_disp
+
+class Curso:
+    def __init__(self, id_curso, nombre, horas, lab, profe_disp, max_alumnos):
+        self.id = id_curso
+        self.nombre = nombre
+        self.horas = horas
+        self.lab = lab
+        # self.profe_disp = profe_disp
+        self.max_alumnos = max_alumnos
+
+class Aula:
+    def __init__(self, numero, edificio, lab, max_alumnos, horarios_disp):
+        self.numero = numero
+        self.edificio = edificio
+        self.lab = lab
+        self.max_alumnos = max_alumnos
+        self.horarios_disp = horarios_disp
+
+class Seccion:
+    def __init__(self, id_seccion, profesor_asignado, horario, dias, curso):
+        self.id = id_seccion
+        self.profesor_asignado = profesor_asignado
+        self.horario = horario
+        self.dias = dias
+        self.curso = curso
+
+
+grafo={
+    'A' : ['B','C'],
+    'B' : ['D','E','F'], 
+    'C' : ['G'],
+    'D' : [],
+    'E' : [],
+    'F' : ['H'],
+    'G' : ['I'],
+    'H' : [],
+    'I' : [],
+}
+
+def bfs(grafo,node):
+    visited = []
+    queueFIFO = []
+
+    visited.append(node)
+    queueFIFO.append(node)
+
+    while queueFIFO:
+        s= queueFIFO.pop(0)
+        print(s, end=' ')
+
+        for n in grafo[s]:
+            if n not in visited:
+                visited.append(n)
+                queueFIFO.append(n)
+
+bfs(grafo,'A')
