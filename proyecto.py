babcecb04a5869e27ad4c5349c1809061abeed5d
Eduardo Mora <99228341+EduardoGMora@users.noreply.github.com>
2023-11-20 09:47:03 -0600

    Update proyecto.py
    lo que hice fue mover las funciones del grafo y agregar una clase llamada bipartito, esto para poder agregar nodos de profesor y curso desde esta clase y las funciones se convirtieron en métodos de la clase

--- proyecto.py
+++ proyecto.py
@@ -2,6 +2,10 @@ import networkx as nx
 import json
 import matplotlib.pyplot as plt
 
+# Cargar datos JSON
+with open('Datos.json', 'r', encoding='utf-8') as file:
+    datos = json.load(file)
+    
 #profesores
 class Profesor:
     def __init__(self, id_profesor, nombre, horarios_disp):
@@ -37,98 +41,85 @@ class Seccion:
         self.dias = dias
         self.curso = curso
 
-# Cargar datos JSON
-with open('Datos.json', 'r', encoding='utf-8') as file:
-    datos = json.load(file)
+#Grafo bipartito
+class Bipartito:
+    def __init__(self,grafo):
+        self.grafo = self.generar_grafo_bipartito(datos)
 
-#muestra todos los profesores disponibles
-def mostrarprofesor():
+    #muestra todos los profesores disponibles
+    def mostrarprofesores(self):
         if 'Profesor' in datos['Datos'][0]:
             profesores = datos["Datos"][0]["Profesor"]
-            if profesores:
-                print('PROFESORES ->')
-                for profesor in profesores:
-                    print(f" ID: {profesor['id']},\n Nombre: {profesor['Nombre']}, \n Horarios disponibles: ")
-                    for horario in profesor['Horarios Disponibles']:
-                        print(f'    - {horario}')
+            print('PROFESORES ->')
+            for profesor in profesores:
+                print(f" ID: {profesor['id']}\n Nombre: {profesor['Nombre']}\n Horarios disponibles: ")
+                for horario in profesor['Horarios Disponibles']:
+                    print(f'    - {horario}')
+                print('')
+        else: print("No hay información de profesores")
 
-                    
-            else: return print("No hay profesores en los datos.")
-        else: print("No hay información sobre profesores en el archivo.")
-mostrarprofesor()
+    def buscarprofe(self):
+        pass
 
-#función para generar grafo bipartito
-def generar_grafo_bipartito(datos):
-    grafo = nx.Graph()
-    
-    #conjunto de datos 'Cursos'
-    cursos = datos["Datos"][2]["Curso"]
-    #se crea un nodo por cada elemento del conjunto
-    for curso in cursos:
-        id_curso = curso["ID"]
-        grafo.add_node(id_curso, bipartite=0, tipo="Curso")
+    def agregarprofe(self):
+        pass
 
-    # Conjunto de datos 'Profesores'
-    profesores = datos["Datos"][0]["Profesor"]
-    #se crea un nodo por cada elemento del conjunto
-    for profesor in profesores:
-        nombre_profesor = profesor['Nombre']
-        grafo.add_node(nombre_profesor, bipartite=1, tipo="Profesor")
+    def borrarprofe(self):
+        pass
 
-    # Agregar aristas al grafo dependiendo cuales profesores estan disponibles para cada materia
-    for curso in cursos:
+    #muestra los cursos disponibles
+    def mostrarCursos(self):
+        if 'Curso' in datos['Datos'][2]:
+            cursos = datos['Datos'][2]['Curso']
+            print('Cursos ->')
+            for curso in cursos:
+                print(f" ID: {curso['ID']}\n Nombre: {curso['Nombre']}\n Horas por semana -> {curso['Cantidad de horas']}\n Laboratorio: {curso['Uso de laboratorio']}")
+                for profesor in curso['Profesores disponibles']:
+                    print(f'    - {profesor}')
+                print(f" Capacidad máxima de alumnos: {curso['Cantidad máxima de alumnos']}\n")
+        else: print("No hay información de los cursos")
+
+    #función para generar grafo bipartito
+    def generar_grafo_bipartito(self, datos):
+        grafo = nx.Graph()
+        
+        #conjunto de datos 'Cursos'
+        cursos = datos["Datos"][2]["Curso"]
+        #se crea un nodo por cada elemento del conjunto
+        for curso in cursos:
+            id_curso = curso["ID"]
+            grafo.add_node(id_curso, bipartite=0, tipo="Curso")
+
+        # Conjunto de datos 'Profesores'
+        profesores = datos["Datos"][0]["Profesor"]
+        #se crea un nodo por cada elemento del conjunto
         for profesor in profesores:
             nombre_profesor = profesor['Nombre']
-            #si el profesor está disponible en ese curso, se crea una arista que los conecta
-            if nombre_profesor in curso["Profesores disponibles"]:
-                id_curso = curso["ID"]
-                grafo.add_edge(nombre_profesor, id_curso)
+            grafo.add_node(nombre_profesor, bipartite=1, tipo="Profesor")
 
-    return grafo
+        # Agregar aristas al grafo dependiendo cuales profesores estan disponibles para cada materia
+        for curso in cursos:
+            for profesor in profesores:
+                nombre_profesor = profesor['Nombre']
+                #si el profesor está disponible en ese curso, se crea una arista que los conecta
+                if nombre_profesor in curso["Profesores disponibles"]:
+                    id_curso = curso["ID"]
+                    grafo.add_edge(nombre_profesor, id_curso)
+        return grafo
 
-def nodos(grafo_bipartito):
-    # esto colorea los nodos de tipo curso de color azul y los demás de rojo
-    colores = ['blue' if grafo_bipartito.nodes[nodo]['tipo'] == 'Curso' else 'red' for nodo in grafo_bipartito.nodes()]
+    def mostrar_nodos(self):
+        # esto colorea los nodos de tipo curso de color azul y los demás de rojo
+        colores = ['blue' if self.grafo.nodes[nodo]['tipo'] == 'Curso' else 'red' for nodo in self.grafo.nodes()]
+        # Dibujar el grafo coloreado
+        pos = nx.circular_layout(self.grafo)
+        nx.draw(self.grafo, pos, with_labels=True, font_weight='bold', node_color=colores)
+        plt.show()
 
-    # Dibujar el grafo coloreado
-    pos = nx.spring_layout(grafo_bipartito)
-    nx.draw(grafo_bipartito, pos, with_labels=True, font_weight='bold', node_color=colores, cmap=plt.cm.rainbow)
-    plt.show()
+def __main__():
+    grafo1 = Bipartito(datos)
+    grafo1.mostrarprofesores()
+    grafo1.mostrarCursos()
+    grafo1.mostrarCursos()
+    grafo1.mostrar_nodos()
 
-# Generar el grafo bipartito
-grafo_bipartito = generar_grafo_bipartito(datos)
-
-#dibuja el grafo
-nodos(grafo_bipartito)
-
-"""
-grafo={
-    'A' : ['B','C'],
-    'B' : ['D','E','F'], 
-    'C' : ['G'],
-    'D' : [],
-    'E' : [],
-    'F' : ['H'],
-    'G' : ['I'],
-    'H' : [],
-    'I' : [],
-}
-
-def bfs(grafo,node):
-    visited = []
-    queueFIFO = []
-
-    visited.append(node)
-    queueFIFO.append(node)
-
-    while queueFIFO:
-        s= queueFIFO.pop(0)
-        print(s, end=' ')
-
-        for n in grafo[s]:
-            if n not in visited:
-                visited.append(n)
-                queueFIFO.append(n)
-
-bfs(grafo,'A')
-"""
+__main__()
